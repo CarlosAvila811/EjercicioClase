82a005ec668f947f65241db5d40e73f877b9c7be
CarlosAvila811 <ca03899@gmail.com>
2024-07-25 21:20:19 -0600

    Agregar archivos de proyecto.

---
+++ EjercicioClase/Program.cs
@@ -0,0 +1,15 @@
+﻿using EjercicioClase;
+//Creando un nuevo opjeto
+
+SuperHeroe superHeroe = new SuperHeroe();
+
+superHeroe.Nombre = "SuperMan ";
+superHeroe.IdentidadSecreta = "Clark";
+superHeroe.Ciudad = "Kirpton";
+superHeroe.SuperPoder.Nombre ="Super Humano";
+superHeroe.SuperPoder.Descripcion = "Supèr fuerza, vision Laser, debilidad La criptonita";
+superHeroe.SuperPoder.Nivel = 9;
+
+superHeroe.Imprimir();
+
+
---
+++ EjercicioClase/SuperHeroe.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace EjercicioClase
+{
+    public class SuperHeroe
+    {
+        public string Nombre { get; set; }
+
+        public string IdentidadSecreta { get; set; }
+
+        public string Ciudad {  get; set; }
+
+        public bool PuedeVolar { get; set; }
+
+        public SuperPoder SuperPoder { get; set; }
+
+        // constructor
+        public SuperHeroe()
+        {
+            PuedeVolar = true;
+            SuperPoder = new SuperPoder();
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"Nombre:{Nombre}");
+            Console.WriteLine($"Identidad Secreta:{IdentidadSecreta}");
+            Console.WriteLine($"Ciudad:{Ciudad}");
+            Console.WriteLine($"Puede Volar:{PuedeVolar}");
+            Console.WriteLine($"Super Poder:{SuperPoder.Nombre } ");
+            Console.WriteLine($"Descripcion:{SuperPoder.Descripcion} ");
+            Console.WriteLine($"Nivel:{SuperPoder.Nivel} ");
+        }
+    }
+}
